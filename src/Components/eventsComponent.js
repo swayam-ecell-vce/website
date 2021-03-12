0e3883e036590af1cf6e203d841bbdd74425c0fd
Sai Jaswanth Gudibandi <jaswanth2k@gmail.com>
2021-03-12 14:17:25 +0530

    event details page added

--- src/Components/eventsComponent.js
+++ src/Components/eventsComponent.js
@@ -1,8 +1,12 @@
 import React from 'react'
+
+//import other components here
 import '../Stylesheets/Events.css'
 
 const eventComponent = (props) =>
 {
+
+  //Need to connect the events with a database and get the details from there using Axios
 	return(
 		<div className='ev-div-main'>
 			<p className='ev-p-head'>
@@ -13,7 +17,7 @@ const eventComponent = (props) =>
 	    <p className='ev-p-title'>Incubation Contest </p>
         <p className='ev-p-date'>21.03.2021</p>
         <p className='ev-p-info'>Ethereum is a technology that's home to digital money, global payments</p>   
-        <p className='ev-p-btn'>Register Now</p>     
+        <a href='/events/1' className='ev-p-btn'>Register Now</a>     
       </div>
       <div className='ev-div-card'>
 	    <p className='ev-p-title'>Incubation Contest </p>
