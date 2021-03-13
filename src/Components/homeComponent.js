4400d6d0447bb0c4055595eba612bebfcbcef3ff
Sai Jaswanth Gudibandi <jaswanth2k@gmail.com>
2021-03-13 12:26:44 +0530

    added: photo collage, testimonials carousel

--- src/Components/homeComponent.js
+++ src/Components/homeComponent.js
@@ -1,28 +1,18 @@
 import React from 'react'
 import '../Stylesheets/HomeContainer.css'
-import styled from 'styled-components'
 import EmojiEventsOutlinedIcon from '@material-ui/icons/EmojiEventsOutlined';
 import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 import PeopleOutlineRoundedIcon from '@material-ui/icons/PeopleOutlineRounded';
 import LocalAtmRoundedIcon from '@material-ui/icons/LocalAtmRounded';
 import EmojiPeopleRoundedIcon from '@material-ui/icons/EmojiPeopleRounded';
 import BusinessRoundedIcon from '@material-ui/icons/BusinessRounded';
-import { Grid, SvgIcon } from '@material-ui/core';
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
+import Slider from "react-slick";
 
-const Div = styled.div `
+//import the css files here
+import "../../node_modules/slick-carousel/slick/slick.css" 
+import "../../node_modules/slick-carousel/slick/slick-theme.css";
 
-height: 50vh;
-width: 70%;
-margin-top: 4%;
-padding: 3%;
-background-image: url(${props => props.urlparam || 'https://images.unsplash.com/photo-1462275646964-a0e3386b89fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'});
-background-size: cover;
-background-repeat: no-repeat;
-background-position: center;
-box-shadow: 10px 10px #a361f3;
-margin-bottom: 3%;
-
-`
 
 const homeComponent = (props) =>
 {
@@ -30,8 +20,18 @@ const homeComponent = (props) =>
 	{
 		//configure the click handler user React.history.push or something better
 	}
+	//trying out the carousel from react-slick
+	const settings = {
+		dots: true,
+		lazyLoad: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		initialSlide: 2
+	  };
 	return(
-		<React.Fragment>
+		<div className='home-wrapper'>
 		<div className='home-div-header'>
 			<p className='home-p-title'>
 			<span className='home-span-Swayam'>//Swayam Logo//</span> <br />
@@ -56,7 +56,58 @@ const homeComponent = (props) =>
 			</div>
 		</div>
 		{/* Add the numbers speak line after this */}
-
+		<div className='home-div-collage'>
+			<div className='home-div-collage-title'>
+				<p className='home-p-collage-title'>
+				Swayam At a Glance
+				</p>
+			</div>
+		<div className="wrapper">
+			<div className="one">
+				<div className='overlay'>
+					<p className='text'>
+						Swayam Internship Drive 2019
+					</p>
+				</div>
+			</div>
+			<div className="two">
+				<div className='overlay'>
+					<p className='text'>
+						Swayam Hackathon
+					</p>
+				</div>
+			</div>
+			<div className="three">
+				<div className='overlay'>
+					<p className='text'>
+						Swayam Ideation Contest 2019
+					</p>
+				</div>
+			</div>
+			<div className="four">
+				<div className='overlay'>
+					<p className='text'>
+						Swayam Start-up Fest 2019
+					</p>
+				</div>
+			</div>
+			<div className="five">
+				<div className='overlay'>
+					<p className='text'>
+						Swayam Service Drive 2019
+					</p>
+				</div>
+			</div>
+			<div className="six">
+				<div className='overlay'>
+					<p className='text'>
+						Swayam Doge
+					</p>
+				</div>
+			</div>
+		</div>
+		</div>
+		{/*test code for the photo collage */}
 		<div className='home-div-numb-title'>
 			Swayam in Numbers
 		</div>
@@ -92,12 +143,60 @@ const homeComponent = (props) =>
 		<div className='home-div-caro-title'>
 			Here’s What Some of Our Prominent Visitors had to Say
 		</div>
-		<div className='home-div-caro'>
-			<Div urlparam = '/1.PNG'>
 
-			</Div>
+		<div className='home-div-show'>
+			<Slider {...settings}>
+				<div className='home-div-show-wrapper'>
+					<div className='home-img-avatar'>
+
+					</div>
+					<div className='home-div-show-quote'>
+						<FormatQuoteIcon style={{ fontSize: 55, color: 'black' }}/>
+					</div>
+					<div className='home-div-show-text'>
+						<br />
+						<p className='home-p-show-text'>
+							You guys  provided young enthusiasts exposure to experts
+							with various initiatives, successes and failures, and that
+							is truly Amazing.
+							<br />
+							<p className='home-p-show-author'>
+								Sri Charan Lakkaraju
+								<br />
+								<p className='home-p-show-title'>
+								Forbes 30 Under30 Asia 2018 | Founder, stuMagz
+								</p>
+							</p>
+						</p>
+					</div>
+				</div>
+				<div className='home-div-show-wrapper'>
+					<div className='home-img-avatar'>
+
+					</div>
+					<div className='home-div-show-quote'>
+						<FormatQuoteIcon style={{ fontSize: 55, color: 'black' }}/>
+					</div>
+					<div className='home-div-show-text'>
+						<br />
+						<p className='home-p-show-text'>
+							You guys  provided young enthusiasts exposure to experts
+							with various initiatives, successes and failures, and that
+							is truly Amazing.
+							<br />
+							<p className='home-p-show-author'>
+								Sri Charan Lakkaraju
+								<br />
+								<p className='home-p-show-title'>
+								Forbes 30 Under30 Asia 2018 | Founder, stuMagz
+								</p>
+							</p>
+						</p>
+					</div>
+				</div>
+			</Slider>
+      	</div>
 		</div>
-		</React.Fragment>
 		
 	)
 }
