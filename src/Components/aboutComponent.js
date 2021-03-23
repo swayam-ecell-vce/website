c966a6dec013d7d67ed83b1b0de94e172f191693
unknown <mssharatchandra@gmail.com>
2021-03-23 23:15:21 +0530

    new changes to about us page withoud video and thumbnail

--- src/Components/aboutComponent.js
+++ src/Components/aboutComponent.js
@@ -97,7 +97,7 @@ Swayam takes curious, insightful, and hard-working students and shapes them into
 						But be aware slick-carousel has a peer-dependancy on jQuery which you,
 						or your colleagues may not like to see in your console output,
 						so you can always grab the CSS from there and convert it
-						into any CSS in JS solution that you might be using.
+						
 						</p>
 					</div>
 				</div>
