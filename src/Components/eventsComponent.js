8ce3337196dfc6b3392ff9b8bd16dad43b687049
Sai Jaswanth Gudibandi <jaswanth2k@gmail.com>
2021-03-24 22:14:01 +0530

    UI changes

--- src/Components/eventsComponent.js
+++ src/Components/eventsComponent.js
@@ -5,11 +5,16 @@ import {useHistory} from 'react-router-dom'
 //import other components here
 import '../Stylesheets/Events.css'
 
-const eventComponent = (props) =>
+const EventComponent = (props) =>
 {
+  var history = useHistory()
   const eventclickhandler = (event) =>
   {
-    console.log(event.target)
+    console.log(event.target.id)
+    const baseurl = '/events/'
+    const eventurl = event.target.id
+    const finalurl = baseurl.concat(eventurl)
+    history.push(finalurl)
   }
   const eventsmapped  = props.eventdb.map((event) => 
   {
@@ -18,12 +23,12 @@ const eventComponent = (props) =>
     const finalurl = baseurl.concat(urlid)
     return(
       <React.Fragment>
-        <div className={event.status ? "ev-div-card pastevent" : "ev-div-card"} onClick = {eventclickhandler}>
-          <p className='ev-p-title'>{event.title} </p>
-          <p className='ev-p-date'>{event.date}</p>
-          <p className='ev-p-info'>{event.tagline}</p>
+        <div className={event.status ? "ev-div-card pastevent" : "ev-div-card"} id={event.title} onClick = {eventclickhandler}>
+          <p className='ev-p-title' id={event.title}>{event.title} </p>
+          <p className='ev-p-date' id={event.title}>{event.date}</p>
+          <p className='ev-p-info' id={event.title}>{event.tagline}</p>
           <div className='ev-btn-grp'>
-            <NavLink to= {finalurl} className="ev-p-btn" exact>More Details</NavLink>
+            <NavLink id={event.title} to= {finalurl} className="ev-p-btn" exact>More Details</NavLink>
             <p className={event.status ? "ev-p-btn disabled" : "ev-p-btn hidden"}>Registrations closed</p>
             <a href ='https://forms.office.com/Pages/ResponsePage.aspx?id=2gmg8pG0u02U6FgJFiVJzUlO1dwao5NGoawk9C6n8HlUMzYxNEQwSlBEUDNBTlpaNjRUS0YySzVDMy4u' className={event.status ? "ev-p-btn ev-btn-register hidden" : "ev-p-btn ev-btn-register"} target='blank'>Register Now</a>
           </div>
@@ -48,4 +53,4 @@ const eventComponent = (props) =>
 	)
 }
 
-export default eventComponent
+export default EventComponent
