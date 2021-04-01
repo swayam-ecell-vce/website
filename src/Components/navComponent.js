3d52b6b1c8ac3804879e3b8f95bd03b29f26c772
NK Pranesh <nkpranesh@gmail.com>
2021-04-01 23:48:12 +0530

    Collapsable Navbar Created

--- src/Components/navComponent.js
+++ src/Components/navComponent.js
@@ -1,10 +1,56 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import '../Stylesheets/Navbar.css'
 
-const navcomponent = (props) =>
+import React, {useState, useEffect} from 'react'
+import {Link, NavLink} from 'react-router-dom'
+
+import '../Stylesheets/Navbar.css'
+import {FaBars} from "react-icons/fa"
+import {AiOutlineClose} from "react-icons/ai";
+
+function useWindowSize() {
+  const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+  useEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
+
+const slidebarData = [
+    {
+        title: 'Swayam',
+        path: '/',
+        cNAme: 'nav-text'
+    },
+    {
+        title: 'Incubator',
+        path: '/incubator',
+        cNAme: 'nav-text'
+    },
+    {
+        title: 'Events',
+        path: '/events',
+        cNAme: 'nav-text'
+    },
+    {
+        title: 'About Us',
+        path: '/about',
+        cNAme: 'nav-text'
+    }
+]
+
+const Navcomponent = (props) =>
 {
-	return(
+	let [width, height]=useWindowSize()
+	const [sidebar, setSidebar]=useState(false)
+	const showSidebar = () => setSidebar(!sidebar)
+
+	if(width > 720)
+	{
+		return(
 		<ul className='Nav-ul-main'>
 			<NavLink to='/events' className='Nav-li' activeClassName='selected' exact>
 				Events
@@ -19,7 +65,39 @@ const navcomponent = (props) =>
 				Swayam
 			</NavLink>
 		</ul>
-	)
+		)
+	}
+	else
+	{
+		return(
+			<>
+				<div className='nav-toggle-main'>
+					<Link to='#' className='toggle-menu-bars'>
+						<FaBars onClick={showSidebar} />
+					</Link>
+					<span className='nav-toggle-title'>TITLE</span>
+				</div>
+				<nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+					<ul className='nav-menu-items' onClick={showSidebar}>
+						<li className='navbar-toggle'>
+							<Link to='#' className='menu-bars'>
+								<AiOutlineClose />
+							</Link>
+						</li>
+						{slidebarData.map((item, index) => {
+							return (
+								<li key={index} className={item.cNAme}>
+									<Link to={item.path}>
+										<span>{item.title}</span>
+									</Link>
+								</li>
+							)
+						})}
+					</ul>
+				</nav>
+			</>
+		)
+	}	
 }
 
-export default navcomponent
+export default Navcomponent
