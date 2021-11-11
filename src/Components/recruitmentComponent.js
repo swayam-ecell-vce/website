b466f8931b6a786f5bb760b40f949afc7eeb3480
imran mirza <imranmirza070901@gmail.com>
2021-11-11 19:01:57 +0530

    Working on colour theme

---
+++ src/Components/recruitmentComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Stylesheets/recruitment.css'
+
+const recruitmentComponent = (props) => {
+
+    return (
+        <div className='container'>
+             
+        </div>
+    )
+    
+};
+export default recruitmentComponent;
---
+++ src/Containers/Recruitment.js
@@ -0,0 +1,14 @@
+import React, { Component, Container } from "react";
+import RecruitmentComponent from "../Components/recruitmentComponent";
+
+class Recruitment extends Component{
+
+    render() {
+        return (
+            <RecruitmentComponent/>
+        )
+    }
+
+}
+
+export default Recruitment;
